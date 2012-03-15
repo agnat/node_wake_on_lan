05559e16fea330a8fc2c0ebe704e8d5e2c642b3e
David Siegel <david.siegel@artcom.de>
2012-03-15 15:57:58 +0100

    defer setBroadcast() until socket is listening and support IPv6 addresses

--- wake_on_lan.js
+++ wake_on_lan.js
@@ -1,31 +1,32 @@
 var dgram  = require('dgram')
+  , net  = require('net')
   , Buffer = require('buffer').Buffer
   ;
 
-var num_mac_octets = 6;
+var mac_bytes = 6;
 
 exports.createMagicPacket = function(mac) {
-  var mac_buffer = new Buffer(num_mac_octets)
+  var mac_buffer = new Buffer(mac_bytes)
     , i
     ;
-  if (mac.length == 2 * num_mac_octets + (num_mac_octets - 1)) {
+  if (mac.length == 2 * mac_bytes + (mac_bytes - 1)) {
     mac = mac.replace(new RegExp(mac[2], 'g'), '');
   }
-  if (mac.length != 2 * num_mac_octets || mac.match(/[^a-fA-F0-9]/)) {
+  if (mac.length != 2 * mac_bytes || mac.match(/[^a-fA-F0-9]/)) {
     throw new Error("malformed MAC address '" + mac + "'");
   }
 
-  for (i = 0; i < num_mac_octets; ++i) {
+  for (i = 0; i < mac_bytes; ++i) {
     mac_buffer[i] = parseInt(mac.substr(2 * i, 2), 16);
   }
 
   var num_macs = 16
-    , buffer   = new Buffer((1 + num_macs) * num_mac_octets);
-  for (i = 0; i < num_mac_octets; ++i) {
+    , buffer   = new Buffer((1 + num_macs) * mac_bytes);
+  for (i = 0; i < mac_bytes; ++i) {
     buffer[i] = 0xff;
   }
   for (i = 0; i < num_macs; ++i) {
-    mac_buffer.copy(buffer, (i + 1) * num_mac_octets, 0, mac_buffer.length)
+    mac_buffer.copy(buffer, (i + 1) * mac_bytes, 0, mac_buffer.length)
   }
   return buffer;
 };
@@ -43,13 +44,12 @@ exports.wake = function(mac, opts, callback) {
     , interval    = opts['interval']    || 100
     , port        = opts['port']        || 9
     , magic_packet = exports.createMagicPacket(mac)
-    , socket = dgram.createSocket('udp4')
+    , socket = dgram.createSocket(net.isIPv6(address) ? 'udp6' : 'udp4')
     , i = 0
     , timer_id
     ;
 
-  socket.setBroadcast(true);
-  function handler(error) {
+  function post_write(error) {
     if (error || i === num_packets) {
       socket.close();
       if (timer_id) {
@@ -62,13 +62,16 @@ exports.wake = function(mac, opts, callback) {
   }
   function sendWoL() {
     i += 1;
-    socket.send(magic_packet, 0, magic_packet.length, port, address, handler);
+    socket.send(magic_packet, 0, magic_packet.length, port, address, post_write);
     if (i < num_packets) {
       timer_id = setTimeout(sendWoL, interval);
     } else {
       timer_id = undefined;
     }
   }
+  socket.once('listening', function() {
+    socket.setBroadcast(true)
+  })
   sendWoL();
 }
 
